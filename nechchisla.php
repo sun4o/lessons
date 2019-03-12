b1c6b506e0192d4e45096b577ec921b5382893fa
sun4o <serg.sych04@mail.ru>
2019-03-12 16:06:47 +0300

    Сделали первую программу

--- nechchisla.php
+++ nechchisla.php
@@ -1,10 +1,47 @@
-<?php
-$j=1;
-do{
-    if($j%2==0)
-    {
-        echo $j."<br>";
-    }
+<form method="post">
+    Вывести числа от <input type="text" name="number1">
+    до <input type="text" name="number2">
+    <select name="type">
+        <option value="2">чётные</option>
+        <option value="1">нечётные</option>
+        <option value="all">все</option>
+    </select>
+    <input type="submit" value="СМЭРТЬ">
+</form>
 
-    $j++;
-}while ($j<100);
+
+<?php
+$j=$_POST['number1'];
+
+switch ($_POST['type'])
+{
+    case 1:
+        do{
+            if($j%2!=0)
+            {
+                echo $j."<br>";
+            }
+
+            $j++;
+        }while ($j<=$_POST['number2']);
+        break;
+    case 2:
+        do{
+            if($j%2==0)
+            {
+                echo $j."<br>";
+            }
+
+            $j++;
+        }while ($j<=$_POST['number2']);
+        break;
+    case 'all':
+        do{
+
+                echo $j."<br>";
+
+            $j++;
+        }while ($j<=$_POST['number2']);
+        break;
+}
+
