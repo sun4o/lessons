e4c0fea8728f9d9047445433675f1f05f4047285
sun4o <serg.sych04@mail.ru>
2019-03-25 20:20:50 +0300

    Я сделал этот дискриминант

---
+++ dis.php
@@ -0,0 +1,39 @@
+
+
+<form method="POST">
+
+    a:<br>
+
+    <input type="text" name="a">
+
+    <br>
+
+    b:<br>
+
+    <input type="text" name="b">
+
+    <br>
+
+    c:<br>
+
+    <input type="text" name="c">
+<br>
+ <input type="submit" value="Посчитать">
+</form>
+
+<?php
+$a=$_POST["a"];
+
+$b=$_POST["b"];
+
+$c=$_POST["c"];
+
+
+
+$d1=$b*$b;
+
+$d=$d1-4*$a*$b;
+
+echo"D=".$d;
+
+?>
