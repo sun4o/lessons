b25c99aef1ec860741f24d5ba116e755bf288e2d
lyubomyr83 <lyubomyr83@mail.ru>
2019-04-16 15:51:56 +0300

    arrays

--- lib/functions.php
+++ lib/functions.php
@@ -4,7 +4,8 @@ function Hello ($n)
     echo "Hello ".$n;
 }
 
-function sum ($a,$b){
+function sum ($a,$b)
+{
     $s=$a+$b;
         echo "Сумма чисел ".$a." и ".$b." равна ".$s;
 }
@@ -24,4 +25,21 @@ function irur ()
 {
    echo sqrt(9);
 
+}
+
+function db ($sql)
+{
+    $connection = mysqli_connect("localhost", 'sergey', 12345, "sergey");
+    mysqli_query($connection,"SET NAMES UTF-8");
+
+   if($sql)
+   {
+       $result = mysqli_query($connection, $sql);
+       while ($row = mysqli_fetch_assoc($result))
+       {
+           $rows[] = $row;
+       }
+
+       return $rows;
+   }
 }
